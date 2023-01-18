317082b99a9492a10b4324f6a0e5ebf8c806e62f
enjesh <enjesh@gmail.com>
2023-01-18 23:55:49 +0300

    Fixed the compiled with errors issue

--- src/GroupedYarns.jsx
+++ src/GroupedYarns.jsx
@@ -2,37 +2,69 @@ import { useState } from "react"
 
 
 const GroupedYarns = ({yarns, selectedSize, setSize}) => {
-    const [groupedSizes, setGroupData] = useState(GroupedYarns)
+    const [groupedSizes, setGroupData] = useState(groupedYarns)
     
-    function GroupedYarns() {
+    function groupedYarns() {
         var sizes = [];
 
-        var sizeSmallYarns = yarns.filter((yarn) => yarn.selectedSize === 'small');
-        var small = {size:'small', yarns: sizeSmallYarns, collapsed: selectedSize === 'small'?false:true}
+        var sizeSmallYarns = yarns.filter((yarn) => yarn.selectedSize === "small");
+        var small = {size:"small", yarns: sizeSmallYarns, collapsed: selectedSize === "small"?false:true}
         yarns.push(small);
 
-        var sizeMediumYarns = yarns.filter((yarn) => yarn.selectedSize === 'medium');
-        var small = {size:'medium', yarns: sizeMediumYarns, collapsed: selectedSize === 'medium'?false:true}
+        var sizeMediumYarns = yarns.filter((yarn) => yarn.selectedSize === "medium");
+        var medium = {size:"medium", yarns: sizeMediumYarns, collapsed: selectedSize === "medium"?false:true}
         yarns.push(medium);
 
-        var sizeSuperChunkyYarns = yarns.filter((yarn) => yarn.selectedSize === 'superChunky');
-        var superChunky = {size:'superChunky', yarns: sizeSuperChunkyYarns, collapsed: selectedSize === 'superChunky'?false:true}
+        var sizeSuperChunkyYarns = yarns.filter((yarn) => yarn.selectedSize === "superChunky");
+        var superChunky = {size:"superChunky", yarns: sizeSuperChunkyYarns, collapsed: selectedSize === "superChunky"?false:true}
         yarns.push(superChunky);
 
-        var sizeChunkyYarns = yarns.filter((yarn) => yarn.selectedSize === 'chunky');
-        var chunky = {size:'chunky', yarns: sizeChunkyYarns, collapsed: selectedSize === 'chunky'?false:true}
+        var sizeChunkyYarns = yarns.filter((yarn) => yarn.selectedSize === "chunky");
+        var chunky = {size:"chunky", yarns: sizeChunkyYarns, collapsed: selectedSize === "chunky"?false:true}
         yarns.push(chunky);
 
     }
 
+    function handleYarnSizeClick(event) {
+        var transformedGroupData = groupedSizes.map((groupedData)=> groupedData.size === event.currentTarget.id 
+                                                            ?{...groupedData,collapsed:!groupedData.collapsed}
+                                                            :groupedData);
+    setGroupData(transformedGroupData)
+    }
+
     return (
 
-        <main className="container">
-            <div className="row justify-content-center mt-3 mb-4">
-                <div className="col-8">
-                    <h1>Grouped Yarns</h1>
-                </div>
-            </div>
+        <main className="container"> 
+            {
+                GroupedYarns.map((item)=> {
+                    return (
+                        <div key={item.groupedSizes} className='card mt-2' style={{cursor:"pointer"}}>
+                            <h4 id={item.groupedSizes} className="card-header text-secondary bg-white" onClick={handleYarnSizeClick}>
+                                Yarn Size: {item.groupedSizes}
+                            </h4>
+                            <div id = {"collapse_" + item.groupedSizes}
+                                      className={item.collapsed === true?"collapsed":""}>
+                                <hr />
+                                {
+                                    item.sizes.map(size => {
+                                        return (
+                                            <div className="mt-2">
+                                                <h5 className="card-title mt-2">
+                                                    <span className="text-dark">Full Name: {yarns.fullname}</span>
+                                                </h5>
+                                                <p>Origin: {yarns.origin}</p>
+                                            </div>
+                                        )
+                                    })
+                                }
+                                
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        
+            h
         </main>
     )
 
